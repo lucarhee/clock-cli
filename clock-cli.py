8392452a32ef8f163f3c5325e7ce0e416ba0c7c2
Luca Rhee <lucarhee@gmail.com>
2023-11-23 13:53:16 +0900

    - clock completed

--- clock-cli.py
+++ clock-cli.py
@@ -35,16 +35,22 @@ def get_now():
 
 
 def print_big_clock(t:tuple) -> None:
-    hour = t[0]
-    minute = t[1]
-    second = t[2]
+    LINES = 5
+    SPACES = " "
+    hour = f"{t[0]:02}"
+    minute = f"{t[1]:02}"
+    second = f"{t[2]:02}"
 
-    if hour < 10:
-        hour = "0" + str(t[0])
-    if minute < 10:
-        minute = "0" + str(t[1])
-    if second < 10:
-        second = "0" + str(t[2])
+    h_digits = (DIGITS[int(hour[0])], DIGITS[int(hour[1])])
+    m_digits = (DIGITS[int(minute[0])], DIGITS[int(minute[1])])
+    s_digits = (DIGITS[int(second[0])], DIGITS[int(second[1])])
+
+    for l in range(LINES):
+        line = h_digits[0][l] + SPACES + h_digits[1][l] + SPACES + DOTS[l] + SPACES +\
+               m_digits[0][l] + SPACES + m_digits[1][l] + SPACES + DOTS[l] + SPACES +\
+               s_digits[0][l] + SPACES + s_digits[1][l]
+        print(line)
+
 
 def print_small_clock(t:tuple) -> None:
     hour = t[0]
@@ -52,6 +58,7 @@ def print_small_clock(t:tuple) -> None:
     second = t[2]
 
     print(f"{hour:02}:{minute:02}:{second:02}")
+
 
 def print_time(t:tuple, big=False) -> None:
     """Print the time on cli"""
@@ -61,15 +68,13 @@ def print_time(t:tuple, big=False) -> None:
         print_small_clock(t)
 
 
-def main():
+def main(big=False):
     while True:
-        try:
-            os.system('clear')
-            now = get_now()
-            print_time(now)
-            time.sleep(1)
-        except EOFError:
-            print("Bye")
+        os.system('clear')
+        now = get_now()
+        print_time(now, big=big)
+        time.sleep(1)
+
 
 if __name__ == "__main__":
-    main()
+    main(True)
